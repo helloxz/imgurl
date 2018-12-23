8a8e8b0b4c10d933e57cc6df5a00e5b90aec1f18
xiaoz <xiaoz93@outlook.com>
2018-12-23 19:47:47 +0800

    修复一处BUG

--- application/models/Insert.php
+++ application/models/Insert.php
@@ -32,25 +32,6 @@
                 exit;
             }
         }
-        //插入安装的默认数据
-        public function default(){
-            //上传限制初始数据
-            // $uplimit_values = array(
-            //     "max_size"      =>  5,
-            //     "number"        =>  10
-            // );
-            // $uplimit_values = json_encode($uplimit_values);
-            // $uplimit = array(
-            //     "name"      =>  "uplimit",
-            //     "values"    =>  $uplimit_values,
-            //     "switch"    =>  "ON"
-            // );
-            // //插入数据
-            // $this->db->insert('options',$uplimit);
-
-            //图片压缩初始数据
-
-        }
         
     }
 ?>
