e2ad3acff4b0570bdb2822ccc042ce2eab692cd8
xiaoz <xiaoz93@outlook.com>
2019-04-10 17:43:55 +0800

    路由重写

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,3 +52,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'home';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
+$route['api/upload'] = 'upload/localhost';
